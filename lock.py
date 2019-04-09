9d4f2452b95486b75b91cc625962693118c6923a
Jerry Chong <jchong@netbase.com>
2019-04-09 17:53:39 +0800

    Fix dependencies, since hass.io doesn't bundle git

--- lock.py
+++ lock.py
@@ -9,7 +9,7 @@ from homeassistant.const import (
     STATE_LOCKED, STATE_UNLOCKED)
 from homeassistant.helpers.typing import ConfigType
 
-REQUIREMENTS = ['https://github.com/zanglang/pysesame2@patch-1#pysesame2==1.0.0']
+REQUIREMENTS = ['https://github.com/zanglang/pysesame2/archive/1.0.0.zip']
 
 ATTR_SERIAL_NO = 'serial'
 
